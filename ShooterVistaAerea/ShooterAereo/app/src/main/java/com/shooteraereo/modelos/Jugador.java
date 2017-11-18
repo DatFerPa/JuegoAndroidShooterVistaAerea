7899e033b247a40e66e8421e3ce4b10b67726fa9
Fernando Palazuelo <DatFerPa@users.noreply.github.com>
2017-11-18 20:36:19 +0100

    Cargador de tiles acabado
    Cargador de los tiles para definir las paredes del mapa

---
+++ ShooterVistaAerea/ShooterAereo/app/src/main/java/com/shooteraereo/modelos/Jugador.java
@@ -0,0 +1,12 @@
+package com.shooteraereo.modelos;
+
+/**
+ * Created by Fernando on 18/11/2017.
+ */
+
+public class Jugador {
+
+
+
+
+}
--- ShooterVistaAerea/ShooterAereo/app/src/main/java/com/shooteraereo/modelos/Nivel.java
+++ ShooterVistaAerea/ShooterAereo/app/src/main/java/com/shooteraereo/modelos/Nivel.java
@@ -2,9 +2,16 @@ package com.shooteraereo.modelos;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.util.Log;
 
 import com.shooteraereo.R;
 import com.shooteraereo.gestores.CargadorGraficos;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Fernando on 18/11/2017.
@@ -14,6 +21,7 @@ public class Nivel {
     private Context context = null;
     private int numeroNivel;
     private Fondo fondo;
+    private Tile[][] mapaTiles;
 
     public boolean inicializado;
 
@@ -29,6 +37,7 @@ public class Nivel {
 
     public void inicializar()throws Exception {
         fondo = new Fondo(context, CargadorGraficos.cargarDrawable(context, R.drawable.background));
+        inicializarMapaTiles();
     }
 
 
@@ -42,6 +51,84 @@ public class Nivel {
     public void dibujar (Canvas canvas) {
         if(inicializado) {
             fondo.dibujar(canvas);
+            dibujarTiles(canvas);
         }
     }
+
+    private void dibujarTiles(Canvas canvas) {
+        for (int y = 0; y < altoMapaTiles(); y++) {
+            for (int x = 0; x < anchoMapaTiles(); x++) {
+                if (mapaTiles[x][y].imagen != null) {
+                    // Calcular la posición en pantalla correspondiente
+                    // izquierda, arriba, derecha , abajo
+
+                    mapaTiles[x][y].imagen.setBounds(
+                            x * Tile.ancho,
+                            y * Tile.altura,
+                            x * Tile.ancho + Tile.ancho,
+                            y * Tile.altura + Tile.altura);
+
+                    mapaTiles[x][y].imagen.draw(canvas);
+                }
+            }
+        }
+    }
+
+    public int anchoMapaTiles(){
+        return mapaTiles.length;
+    }
+
+    public int altoMapaTiles(){
+
+        return mapaTiles[0].length;
+    }
+
+
+    private void inicializarMapaTiles() throws Exception {
+        InputStream is = context.getAssets().open(numeroNivel+".txt");
+        int anchoLinea;
+
+        List<String> lineas = new LinkedList<String>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        {
+            String linea = reader.readLine();
+            anchoLinea = linea.length();
+            while (linea != null)
+            {
+                lineas.add(linea);
+                if (linea.length() != anchoLinea)
+                {
+                    Log.e("ERROR", "Dimensiones incorrectas en la línea");
+                    throw new Exception("Dimensiones incorrectas en la línea.");
+                }
+                linea = reader.readLine();
+            }
+        }
+
+        // Inicializar la matriz
+        mapaTiles = new Tile[anchoLinea][lineas.size()];
+        // Iterar y completar todas las posiciones
+        for (int y = 0; y < altoMapaTiles(); ++y) {
+            for (int x = 0; x < anchoMapaTiles(); ++x) {
+                char tipoDeTile = lineas.get(y).charAt(x);//lines[y][x];
+                mapaTiles[x][y] = inicializarTile(tipoDeTile,x,y);
+            }
+        }
+    }
+
+    private Tile inicializarTile(char codigoTile,int x, int y) {
+        switch (codigoTile) {
+            case '.':
+                // en blanco, sin textura
+                return new Tile(null, Tile.PASABLE);
+            case '#':
+                // bloque de musgo, no se puede pasar
+                return new Tile(CargadorGraficos.cargarDrawable(context,
+                        R.drawable.piedra), Tile.SOLIDO);
+            default:
+                //cualquier otro caso
+                return new Tile(null, Tile.PASABLE);
+        }
+    }
+
 }
